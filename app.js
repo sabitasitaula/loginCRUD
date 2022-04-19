8a2f26f489bf0a7a454859a29381e0f18069e8ed
Sabita Sitaula <sabitasitaula705@gmail.com>
2022-04-19 17:00:40 +0545

    database connected

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import conn from './db/conn.js';
+// import userRouter from './routes/Users.js';
+
+const app = express();
+conn;
+
+app.use(express.json());
+// app.use("/user",userRouter)
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Hello from my-express-app!"});
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
